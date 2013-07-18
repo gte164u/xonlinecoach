045bfa8304b4cf530ba8b2c3da91e731c47f6d93
Adrienne Johnston <adriennejohnston@me.com>
2013-07-18 14:05:34 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onlinecoach::Application.config.secret_token = 'f9b98c5e37db0c23fc7e11f68d3cc46d22ebc7bdbbe49fa2ec6adeb71682fc65e3a0a56f4741f283c9b8b896e3640e9aba0c14d80c6ae34faec6310bc28b1dea'
